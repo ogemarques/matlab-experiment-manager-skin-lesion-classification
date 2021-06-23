909f3d09b8b2dea33479c69bd53a7dc1cec7aff3
Oge Marques <oge@Oges-MacBook-Pro-2.local>
2021-06-23 07:45:38 -0400

    Cleaning up...

--- utils/makeFolders.m
+++
@@ -1,9 +0,0 @@
-%% Build folders for training images based on zip files from ISBI2016 ISIC
-% Part 3
-
-%% Go to 'root' folder
-cd '/Users/oge/Projects/MATLAB/SkinLesion'
-
-%% Create folders
-mkdir benign
-mkdir malignant
